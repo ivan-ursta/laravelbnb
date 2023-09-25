c38cb0e95a826564b71f3627f25e90cce604464d
Ivan Ursta <ursta2017@gmail.com>
2023-09-25 20:06:20 +0300

    Added new features

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables";
-import Example2 from "./components/Example2";
+import Bookable from "./bookable/Bookable";
+import Review from "./review/Review";
 
 const routes = [{
         path: "/",
@@ -8,10 +9,15 @@ const routes = [{
         name: "home",
     },
     {
-        path: "/second",
-        component: Example2,
-        name: "second",
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable",
     },
+    {
+        path: "/review/:id",
+        component: Review,
+        name: "review",
+    }
 ];
 
 const router = new VueRouter({
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+//  Route::get('bookables', 'Api\BookableController@index');
+//  Route::get('bookables/{id}', 'Api\BookableController@show');
+
+Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show']);
+
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
+Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')
+    ->name('bookables.reviews.index');
+
+Route::get('/booking-by-review/{reviewKey}', 'Api\BookingByReviewController')
+    ->name('booking.by-review.show');
+
+Route::apiResource('reviews', 'Api\ReviewController')->only(['show', 'store']);
+
