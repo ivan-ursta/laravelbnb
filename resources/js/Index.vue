3582c748653f584f7d634f34d334b8a781be2087
Ivan Ursta <ursta2017@gmail.com>
2023-09-28 19:51:12 +0300

    Added some animation and vue code

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -2,7 +2,6 @@
     <div>
         <nav class="navbar bg-white border-bottom navba-light">
             <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }">LaravelBnb</router-link>
-            <router-link class="btn nav-button" :to="{ name: 'second' }">Second</router-link>
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
@@ -11,3 +10,36 @@
   
     </div>
 </template>
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      lastSearch: this.$store.state.lastSearch
+    };
+  },
+  computed: {
+    ...mapState({
+      lastSearchComputed: "lastSearch",
+      isLoggedIn: "isLoggedIn"
+    }),
+    ...mapGetters({
+      itemsInBasket: "itemsInBasket"
+    }),
+    somethingElse() {
+      return 1 + 2;
+    }
+  },
+  methods: {
+    async logout() {
+      try {
+        axios.post("/logout");
+        this.$store.dispatch("logout");
+      } catch (error) {
+        this.$store.dispatch("logout");
+      }
+    }
+  }
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
 import router from "./routes";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import Index from "./Index";
 import moment from "moment";
 
@@ -15,8 +16,9 @@ import StarRating from "./shared/components/StarRating";
 import FatalError from "./shared/components/FatalError";
 import Success from "./shared/components/Success";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import storeDefinition from "./store";
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,15 +31,18 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
-Vue.filter("fromNow", value => moment(value).fromNow())
+Vue.filter("fromNow", (value) => moment(value).fromNow());
 
 Vue.component("star-rating", StarRating);
 Vue.component("fatal-error", FatalError);
 Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors);
+
+const store = new Vuex.Store(storeDefinition);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -45,9 +50,14 @@ Vue.component("v-errors", ValidationErrors);
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     router,
+    store,
     components: {
-        "index": Index
-    }
+        index: Index,
+    },
+    beforeCreate() {
+        this.$store.dispatch("loadStoredState");
+        this.$store.dispatch("loadUser");
+    },
 });
